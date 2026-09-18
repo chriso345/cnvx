873cce75fc18e833ef90c32f12b80658704f2019
Chris <chrisoliver345@gmail.com>
2026-09-18 15:20:19 +1200

    chore: remove self-dependency

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,14 +2,20 @@
 
 set -e
 
+run_clippy() {
+  local fix_mode=$1
+  cargo +nightly clippy --workspace --all-targets \
+        $1 --                                     \
+        -D warnings
+}
+
 if [ "$1" = "--pre-commit" ]; then
   if ! rustup toolchain list | grep --silent 'nightly'; then
     echo "Nightly toolchain is not installed. Please install it with 'rustup toolchain install nightly' and try again."
     exit 1
   fi
 elif [ "$1" = "--fix" ]; then
-  cargo +nightly clippy --workspace --all-targets --fix --allow-dirty -- -D warnings
+  run_clippy "--fix --allow-dirty"
 fi
 
-# Default to checking linting
-cargo +nightly clippy --workspace --all-targets -- -D warnings
+run_clippy
